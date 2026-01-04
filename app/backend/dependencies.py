e37059e6ea5e4e23ab577be8c788e5210513a0a0
v14designv-dotcom <v14designv@gmail.com>
2026-01-04 19:05:08 +0100

    fix

--- app/backend/dependencies.py
+++ app/backend/dependencies.py
@@ -14,9 +14,11 @@ ROOT_DIR = Path(__file__).parent
 load_dotenv(ROOT_DIR / '.env')
 
 # Database dependency
-mongo_url = os.environ['MONGO_URL']
+mongo_url = os.environ.get('MONGO_URL', 'mongodb://localhost:27017')
+db_name = os.environ.get('DB_NAME', 'initium_db')
+
 client = AsyncIOMotorClient(mongo_url)
-db = client[os.environ['DB_NAME']]
+db = client[db_name]
 
 security = HTTPBearer()
 
